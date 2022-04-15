01894325f82f5c6a43db3c25fb126dcbe2eea50f
AlexandrPRG <irdsbk@bk.ru>
2022-04-15 19:09:12 +0300

    urls urlpatterns

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index, contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', index),
+    path('contacts/', contacts),
     path('products/', include('mainapp.urls'))
 ]
