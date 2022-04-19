12480bf67fefd69f1919a3541eef0deb1b78d0c2
AlexandrPRG <irdsbk@bk.ru>
2022-04-19 19:45:24 +0300

    views mainapp

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-# from .views import index, contacts
+from .views import index, contacts
 
 urlpatterns = [
     # path('', index),
-    # path('contacts/', contacts),
-    # path('', include('mainapp.urls'))
+    path('contacts/', contacts),
+    path('', include('mainapp.urls'))
 ]
