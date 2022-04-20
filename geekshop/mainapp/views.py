3710dc7aa588951b4d6a7727d97142f629d5e72b
AlexandrPRG <irdsbk@bk.ru>
2022-04-20 19:32:05 +0300

    creating run.bat

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 
 
 def products(request):
+    print(request, 'mainapp/products.html')
     return render(request, 'mainapp/products.html')
-
-
-def index():
-    return None
-
-
-def contacts():
-    return None
