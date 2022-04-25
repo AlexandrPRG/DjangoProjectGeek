da9daa01a3d9942c5d21d2bb419d2027ea702db4
AlexandrPRG <irdsbk@bk.ru>
2022-04-25 14:50:18 +0300

    start task3 - media

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'geekshop', 'static')]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'geekshop', 'media')
+
