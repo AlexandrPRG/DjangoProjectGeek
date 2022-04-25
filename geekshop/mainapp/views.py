70701a752f06ae9a60019f09f63c06f026731f81
AlexandrPRG <irdsbk@bk.ru>
2022-04-25 20:56:14 +0300

    mainapp views add context

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu_products = [
+        {'href': '', 'name': 'все'},
+        {'href': '', 'name': 'дом'},
+        {'href': '', 'name': 'офис'},
+        {'href': '', 'name': 'модерн'},
+        {'href': '', 'name': 'классика'},
+    ]
+    context = {
+        "links_menu_products": links_menu_products,
+        "title": 'каталог',
+    }
+    return render(request, 'mainapp/products.html', context=context)
