79f93e83cc98c388639de6edce80462bed6f9432
AlexandrPRG <irdsbk@bk.ru>
2022-04-25 15:56:40 +0300

    products.html: edit title from views

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+links_menu_products = [
+    {'href': '', 'name': 'все'},
+    {'href': '', 'name': 'дом'},
+    {'href': '', 'name': 'офис'},
+    {'href': '', 'name': 'модерн'},
+    {'href': '', 'name': 'классика'},
+]
+context = {
+    "links_menu_products": links_menu_products,
+    "title": 'каталог',
+}
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context=context)
