57b1ebc498e491be2282164ec63f641dd3293903
AlexandrPRG <irdsbk@bk.ru>
2022-04-25 16:51:17 +0300

    start

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
-links_menu_products = [
-    {'href': '', 'name': 'все'},
-    {'href': '', 'name': 'дом'},
-    {'href': '', 'name': 'офис'},
-    {'href': '', 'name': 'модерн'},
-    {'href': '', 'name': 'классика'},
-]
-context = {
-    "links_menu_products": links_menu_products,
-    "title": 'каталог',
-}
 
 def products(request):
+    links_menu_products = [
+        {'href': '', 'name': 'все'},
+        {'href': '', 'name': 'дом'},
+        {'href': '', 'name': 'офис'},
+        {'href': '', 'name': 'модерн'},
+        {'href': '', 'name': 'классика'},
+    ]
+    context = {
+        "links_menu_products": links_menu_products,
+        "title": 'каталог',
+    }
     return render(request, 'mainapp/products.html', context=context)
