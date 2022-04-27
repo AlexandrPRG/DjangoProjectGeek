8562053b520f2c0421dd27e3ccd949c0cb2c312d
AlexandrPRG <irdsbk@bk.ru>
2022-04-27 18:10:21 +0300

    mainapp/models.py create

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('products/', include('mainapp.urls'))
 ]
 if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
