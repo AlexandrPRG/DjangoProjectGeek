9c8fec4c203c56264e2aff4265546896063049af
AlexandrPRG <irdsbk@bk.ru>
2022-04-28 23:29:52 +0300

    Заполнение Products

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -23,4 +23,18 @@ class Product(models.Model):
         max_length=60,
         blank=True
         )
-    description = models.TextField(verbose_name='описание', blank=True)
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True
+        )
+    price = models.DecimalField(
+        verbose_name='цена',
+        max_digits=8,
+        decimal_places=2,
+        default=0
+        )
+
+    def __str__(self):
+        return f'{self.name} - {self.category.name}'
+
+
