09adde84211db6c4c46442b194548d09e072406c
AlexandrPRG <irdsbk@bk.ru>
2022-05-01 13:30:08 +0300

    namespace add to geek/urls.py

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('products/', include('mainapp.urls', namespace='products'))
+    path('products/', include('mainapp.urls', namespace='product'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
