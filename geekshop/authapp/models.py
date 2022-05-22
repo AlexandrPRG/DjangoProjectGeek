acd48ec1fdcd35b404f282e5d4965add0045d329
AlexandrPRG <irdsbk@bk.ru>
2022-05-22 13:43:57 +0300

    2.2 profile form 1.28

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -4,11 +4,13 @@ import django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_ava', blank=True)
-    age = models.PositiveIntegerField(verbose_name='возраст')
+    age = models.PositiveIntegerField(verbose_name='возраст', default=18)
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(default=now() + timedelta(hours=48))
 
@@ -17,3 +19,44 @@ class ShopUser(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'm'
+    FEMALE = 'w'
+    GENDER_CHOICES = (
+        (MALE, 'м'),
+        (FEMALE, 'ж')
+    )
+    user = models.OneToOneField(
+        ShopUser,
+        unique=True,
+        null=False,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    tagline = models.CharField(
+        verbose_name='тэги',
+        max_length=128,
+        blank=True,
+    )
+    about_me = models.TextField(
+        verbose_name='о себе',
+        max_length=512,
+        blank=True,
+    )
+    gender = models.CharField(
+        verbose_name='пол',
+        choices=GENDER_CHOICES,
+        max_length=1,
+    )
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
+        pass
