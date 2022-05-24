85bbdfc27b868c021fedeeaa9ea2c9a47634820b
AlexandrPRG <irdsbk@bk.ru>
2022-05-24 09:05:34 +0300

    3.1 orderapp models.py created 50.54

---
+++ geekshop/ordersapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+import ordersapp.views as ordersapp
+
+app_name = 'ordersapp'
+urlpatterns = [
+    # path('', ordersapp.orders, name='orders'),
+    # path('add/<int:pk>/', basketapp.basket_add, name='add'),
+    # path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
+    # path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
+    ]
